a8400f3c92f029b1b93d4045485af4b92745aef8
mpairwe7 <mpairwelaubn75@gmail.com>
2026-02-02 11:17:40 +0300

    added deployed url

--- server/index.js
+++ server/index.js
@@ -46,6 +46,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const allowedOrigins = [
   'http://localhost:3000',
   'https://delipucashserver.vercel.app',
+  'https://mpairwe7-delipucashlatest.vercel.app/',
   'http://localhost:8081',
   'exp://192.168.0.117:8081',
   process.env.FRONTEND_URL // Add your production frontend URL
